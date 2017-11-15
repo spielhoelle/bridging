fad6b881fd901ba84c908d6df5b870e8ae5d2f5f
Thomas Kuhnert <spiel.hoelle@live.de>
2017-11-15 13:16:24 +0100

    give menu correct name

--- wp-content/themes/bridging-ideas/functions.php
+++ wp-content/themes/bridging-ideas/functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'bridging_ideas_setup' ) ) :
 
     // This theme uses wp_nav_menu() in one location.
     register_nav_menus( array(
-      'menu-1' => esc_html__( 'Primary', 'bridging-ideas' ),
+      'primary-menu' => esc_html__( 'Primary', 'bridging-ideas' ),
     ) );
 
     /*
--- wp-content/themes/bridging-ideas/header.php
+++ wp-content/themes/bridging-ideas/header.php
@@ -13,57 +13,57 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
 
 
-	<?php wp_head(); ?>
+        <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> data-target="#navbar-example2" data-spy="scroll" >
 <div id="page" class="site">
-	<!--<a class="skip-link screen-reader-text" href="#content">--><?php //esc_html_e( 'Skip to content', 'bridging-ideas' ); ?><!--</a>-->
+        <!--<a class="skip-link screen-reader-text" href="#content">--><?php //esc_html_e( 'Skip to content', 'bridging-ideas' ); ?><!--</a>-->
 
-	<header id="masthead" class="site-header">
+        <header id="masthead" class="site-header">
 
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-			?>
-		</div><!-- .site-branding -->
-		<nav class="nav navbar navbar-expand-lg navbar-light bg-white fixed-top" id="navbar-example2">
-			<div class="container">
+                <div class="site-branding">
+<?php
+the_custom_logo();
+if ( is_front_page() && is_home() ) : ?>
+                                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                        <?php else : ?>
+                                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+<?php
+endif;
+?>
+                </div><!-- .site-branding -->
+                <nav class="nav navbar navbar-expand-lg navbar-light bg-white fixed-top" id="navbar-example2">
+                        <div class="container">
   <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' );the_custom_logo(); ?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<?php
-			$defaults = array(
-				'menu' => '',
-				'menu_class' => 'menu navbar-nav',
-				'menu_id' => '',
-		    'echo' => true,
-				'fallback_cb' => 'wp_page_menu',
-				'items_wrap' => '<ul  class="%2$s">%3$s</ul>',
-				'item_spacing' => 'preserve',
-		    'depth' => 0,
-				'walker' => '',
-				'theme_location' => '' );
-				wp_nav_menu($defaults);
-			?>
+                <div class="collapse navbar-collapse" id="navbarNav">
+<?php
+$defaults = array(
+  'menu' => '',
+  'menu_class' => 'menu navbar-nav',
+  'menu_id' => '',
+  'echo' => true,
+  'fallback_cb' => 'wp_page_menu',
+  'items_wrap' => '<ul  class="%2$s">%3$s</ul>',
+  'item_spacing' => 'preserve',
+  'depth' => 0,
+  'walker' => '',
+  'theme_location' => '' );
+wp_nav_menu($defaults);
+?>
 
-		</div>
+                </div>
  </div>
 </nav>
 
-	</header><!-- #masthead -->
+        </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+        <div id="content" class="site-content">
