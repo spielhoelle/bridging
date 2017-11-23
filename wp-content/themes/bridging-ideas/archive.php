f1b09dc1b2a75be9087c6c5fb0d29ba6005ec2cb
Thomas Kuhnert <spiel.hoelle@live.de>
2017-11-23 10:38:55 +0100

    archive + fontsize

--- wp-content/themes/bridging-ideas/archive.php
+++ wp-content/themes/bridging-ideas/archive.php
@@ -9,8 +9,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="content-area py-5">
+		<main id="main" class="site-main container">
 
 		<?php
 		if ( have_posts() ) : ?>
@@ -31,7 +31,7 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				get_template_part( 'template-parts/content', 'post' );
 
 			endwhile;
 
